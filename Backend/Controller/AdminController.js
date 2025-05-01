9f1840de330147464abadcb0bd285d16fb68fbdf
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-05-01 19:22:48 +0530

    new

--- Backend/Controller/AdminController.js
+++ Backend/Controller/AdminController.js
@@ -53,17 +53,6 @@ exports.getAllStatesFromStates = async(req, res) => {
 }
 
 
-// exports.getAllCitiesWithStates = async (req, res) => {
-//   try {
-//     const cities = await adminmodel.find().populate('state'); // populate state from AddState
-//     res.status(200).json({ data: cities });
-//   } catch (error) {
-//     console.error('Error fetching cities:', error);
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
-
-
 
   
 
